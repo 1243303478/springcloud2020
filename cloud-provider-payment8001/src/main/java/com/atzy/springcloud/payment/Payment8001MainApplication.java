1744ef8c0f39f2e2e9b1888c4c2d75863459794f
zy <1243303478@qq.com>
2021-02-15 20:06:16 +0800

    [新增]将订单和支付服务注册进入eureka注册中心

--- cloud-provider-payment8001/src/main/java/com/atzy/springcloud/payment/Payment8001MainApplication.java
+++ cloud-provider-payment8001/src/main/java/com/atzy/springcloud/payment/Payment8001MainApplication.java
@@ -2,8 +2,10 @@ package com.atzy.springcloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001MainApplication.class,args);
