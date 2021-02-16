06565c74645fe86a0a23bc4bfaff9ed3675ad283
zy <1243303478@qq.com>
2021-02-17 03:37:28 +0800

    [新增]订单服务8004 注册中心切换为zookeeper中注册服务

---
+++ cloud-provider-payment8004/src/main/java/com/atzy/springcloud/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.atzy.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String server_port;
+
+    @GetMapping("payment/zk")
+    public String paymentZk(){
+        return "SpringCloud with zookeeper:" + server_port + "\t" + UUID.randomUUID().toString();
+    }
+}
