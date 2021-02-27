76b49e8778c042c518bfa5751340ebea37c562c7
zy <1243303478@qq.com>
2021-02-28 02:01:36 +0800

    [新增]初始化分布式配置中心 成功读取到git中Yml配置文件

---
+++ cloud-config-center-3344/src/main/java/com/atzy/springcloud/CloudConfigServer3344Main.java
@@ -0,0 +1,15 @@
+package com.atzy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class CloudConfigServer3344Main {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigServer3344Main.class,args);
+    }
+}
