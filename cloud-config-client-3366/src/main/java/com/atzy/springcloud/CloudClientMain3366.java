1e497e9b8a1e19153a0b00869d17c400ce357abc
zy <1243303478@qq.com>
2021-02-28 04:01:29 +0800

    [新增]加入消息总线 使用rabbitMQ进行消息推送 达到一次推送 处处更新的效果 也可以单独指定那几个服务进行更新

---
+++ cloud-config-client-3366/src/main/java/com/atzy/springcloud/CloudClientMain3366.java
@@ -0,0 +1,13 @@
+package com.atzy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudClientMain3366.class,args);
+    }
+}
