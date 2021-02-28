00b896bd0fdeb1123ade87628972a637a2afdb16
zy <1243303478@qq.com>
2021-02-28 16:21:46 +0800

    [新增]初始化消息驱动消费者 初始化启动类以及配置文件

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atzy/sprincloud/CloudStreamConsumerMain8802.java
@@ -0,0 +1,11 @@
+package com.atzy.sprincloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamConsumerMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamConsumerMain8802.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atzy/springcloud/CloudStreamConsumerMain8803.java
@@ -0,0 +1,11 @@
+package com.atzy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStreamConsumerMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStreamConsumerMain8803.class,args);
+    }
+}
